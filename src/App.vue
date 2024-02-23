b6fed2d477289efef30db9bad4a4b1a0dbd6eafa
ermilov.dima <ermilovdima49@gmail.com>
2024-02-23 09:23:30 +0500

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,160 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- header -->
+    <header class="ds-header" id="site-header">
+        <div class="container">
+            <div class="ds-header-inner">
+              <!-- logo -->
+              <a href="index.html" class="ds-logo">
+                <span>Д</span>Дима Светлов
+              </a>
+              <!-- logo -->
+              <!-- social -->
+              <ul class="ds-social">
+                <li><a href="https://t.me/DmitryErmilov0" target="_blank"><i class="ri-telegram-line"></i></a></li>
+              </ul>
+              <!-- social -->
+            </div>
+        </div>
+    </header>
+    <!-- header -->
+    <!-- banner -->
+    <div class="ds-banner">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 col-xxl-5">
+            <figure>
+              <img src="./assets/images/profile-image.jpg">
+            </figure>
+          </div>
+          <div class="col-12 col-sm-12 col-md-7 col-lg-7 col-xl-7 col-xxl-7">
+            <section>
+                <h1>
+                  <span>Поздравляю</span>
+                  Диму Светлова <br> Лучшего парня
+                </h1>
+                <ul class="ds-numbervalulist">
+                  <li>
+                    <strong>6+</strong>
+                    <span>Лет вместе</span>
+                  </li>
+                  <li>
+                    <strong>89</strong>
+                    <span>Матов в день</span>
+                  </li>
+                  <li>
+                    <strong>24</strong>
+                    <span>Года</span>
+                  </li>
+                </ul>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- banner -->
+
+    <!-- about -->
+    <div class="ds-about-section">
+      <div class="container">
+        <section>
+            <h2 class="ds-heading">О Диме</h2>
+            <p>Дима – это настоящий хамелеон! Он может перенять любой цвет в компании, чтобы подстроиться под настроение других. Но не переживайте, он такой мастер мимикрии, что его настоящее лицо всегда остается загадкой. В общем, если захотите провести весело время и удивиться его способностям, позвать Диму в компанию – гарантировано не пожалеете!</p>
+        </section>
+      </div>
+    </div>
+    <!-- about -->
+
+    <!--  Skills -->
+    <div class="ds-skills-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+             <h2 class="ds-heading">В чём хорош</h2>
+             <ul class="ds-skills-list">
+               <li>Игры</li>
+               <li>Сериалы</li>
+               <li>Истории</li>
+               <li>Прогулки</li>
+               <li>Бубнёж</li>
+               <li>Ворчание</li>
+               <li>Недовольство</li>
+               <li>Отмазкам</li>
+               <li>Лежанию</li>
+             </ul>
+          </div>
+          <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+            <h2 class="ds-heading">Ужасен</h2>
+            <ul class="ds-skills-list">
+              <li>Готовке</li>
+              <li>Чистке</li>
+              <li>Уборке</li>
+              <li>Комплиментах</li>
+              <li>Анекдотах</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--  Skills -->
+
+   <!-- Experience -->
+   <div class="ds-experience-section">
+     <div class="container">
+        <h2 class="ds-heading">Лучшие моменты</h2>
+        <div class="row ds-experience-list">
+            <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+              <section>
+                <span class="ds-year">2019 год</span>
+                <h3 class="ds-officename">Встретились в институте</h3>
+                <span class="ds-department">УРЖТ</span>
+                <p>В первый день учебы в институте я опоздала на пару минут и уже попала на штормовое собрание группы. Оказалось, что это был Дима, который также опоздал и он оказался моим одногруппником.</p>
+                <ul>
+                  <li>С тех пор мы проводили вместе каждый день в институте</li>
+                  <li>Мы потерялись на учебном корпусе и были вынуждены провести там целый вечер</li>
+                </ul>
+              </section>
+            </div>
+            <div class="col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+              <section>
+                <span class="ds-year">2020 - 2024</span>
+                <h3 class="ds-officename">Совместное проживание</h3>
+                <span class="ds-department">Москва </span>
+                <p>Дима, любит готовить, а Мария была отличным кулинарным критиком. Они проводили вечера за разговорами и уютными ужинами, делясь новыми рецептами и планами на будущее.</p>
+                <ul>
+                  <li>Несмотря на все комические ситуации, Мария и Иван были неразлучными друзьями</li>
+                  <li>Их однокомнатная квартира стала их маленьким уютным миром, где царило веселье и дружба.</li>
+                </ul>
+              </section>
+            </div>
+        </div>
+     </div>
+   </div>
+    <!-- Experience -->
+
+   <!--  Work -->
+
+   <!--  footer -->
+   <footer class="ds-footer text-center">
+     <div class="container">
+        <section>
+          <!-- <span>Конец</span> -->
+          <h4>Поздравляю</h4>
+          <p>Диму Светлова</p>
+        </section>
+     </div>
+   </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+@import url("@/assets/css/main.css");
 </style>
